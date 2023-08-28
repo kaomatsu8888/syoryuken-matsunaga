1548c105561d616cda7e9d0517e3a8a6fe8c44bd
Kaoru Matsunaga <m.kaoru0830@gmail.com>
2023-08-29 03:10:52 +0900

    (完成)ファイル名ミスりまして「syo-ryu-ken 」から「syo-ryu-ken-kaoru」へ

--- syoryuken/ViewController.swift
+++ syoryuken/ViewController.swift
@@ -4,16 +4,33 @@
 //
 //  Created by kaoru matsunaga on 2023/08/28.
 //
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet var textLabel: UILabel!
+    // 配列
+    var imageAttack : Array<UIImage> = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // ファイル1から20までアタックファイルを開く
+        for i in 1...20 {
+            if let attackImage = UIImage(named: "attack\(i)") {
+                imageAttack.append(attackImage)
+            }
+        }
     }
-
-
+    @IBAction func buttun(_ sender: Any) {
+        imageView.animationImages = imageAttack
+        // 1.5秒間隔
+        imageView.animationDuration = 2
+        // 1回繰り返し
+        imageView.animationRepeatCount = 1
+        // アニメーションを開始
+        imageView.startAnimating()
+    }
 }
-
